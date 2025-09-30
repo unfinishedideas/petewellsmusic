c0e7102a60e77dee60c4ffb7bd0d1610abb9d3a7
unfinishedideas <unfinishedideas@gmail.com>
2025-09-29 17:28:13 -0700

    created initial color scheme, set up theme variables for colors

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar: React.FC = () => {
+  return (
+    <div className="flex bg-secondary w-full h-lg bg-zmoney">
+      <h1>Pete Wells Music</h1>
+      <p>This is where the navbar is</p>
+    </div>
+  );
+};
+
+export default Navbar;
